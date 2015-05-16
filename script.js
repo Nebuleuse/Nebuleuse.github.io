5ed7f7e9a19c809365639948e31d79572d6f2b25
Orygin <ancien9@gmail.com>
2015-05-16 16:33:42 +0200

    Added download page and documentation

--- script.js
+++ script.js
@@ -8,10 +8,30 @@ angular.module('nebuleuse', [ 'ngRoute' ])
       when('/features', {
         templateUrl: 'tpl/features.html'
       }).
-      when('/documentation', {
+      when('/download', {
+        templateUrl: 'tpl/download.html'
+      }).
+      when('/doc', {
         templateUrl: 'tpl/doc.html'
+      }).
+      when('/doc/:page', {
+        templateUrl: 'tpl/doc_page.html',
+        controller: 'docMarkdown'
       }).
       otherwise({
         redirectTo: '/overview'
       });
-  }]);
+  }])
+.controller('docMarkdown', ['$scope', '$routeParams', function ($scope, $routeParams) {
+  switch ($routeParams.page) {
+    case "api":
+    $scope.pageTitle = "Nebuleuse API"
+    break;
+    case "internals":
+    $scope.pageTitle = "Nebuleuse internals"
+    break;
+    case "cppclient":
+    $scope.pageTitle = "C++ client"
+    break;
+  }
+}]);
